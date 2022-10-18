746daee24ff188ea1ad496c3ab1370f971def8da
ShiCheng Lu <shichenglu0708@gmail.com>
2022-10-18 00:06:59 -0400

    ???

--- frontend/src/main/kotlin/Models/Board.kt
+++ frontend/src/main/kotlin/Models/Board.kt
@@ -1,7 +1,14 @@
-package Models
+package models
 
-class Board(var name: String, var creator: User) {
-    var todos: MutableList<ToDoItem> = mutableListOf<ToDoItem>()
-    var labels: MutableList<String> = mutableListOf<String>()
-    var users: MutableList<User> = mutableListOf<User>(creator)
+import kotlinx.serialization.Serializable
+import java.util.*
+
+@Serializable
+data class Board(
+    val name: String,
+    val users: MutableSet<@Serializable(with = UUIDSerializer::class) UUID>,
+    val id: @Serializable(with = UUIDSerializer::class) UUID,
+    val labels: MutableSet<Label>
+) {
+    constructor(name: String) : this(name, mutableSetOf(), UUID.randomUUID(), mutableSetOf())
 }
