5ff4718000e95ad1e106a3db1941c57067273dad
Neel Ismail <n7ismail@uwaterloo.ca>
2022-10-21 03:23:16 -0400

    Resolve "Create To-Do List Row"

--- frontend/src/main/kotlin/views/BoardView.kt
+++ frontend/src/main/kotlin/views/BoardView.kt
@@ -1,17 +1,29 @@
 import javafx.geometry.*
-import javafx.scene.control.*
-import javafx.scene.input.MouseEvent
+import javafx.scene.control.Label
 import javafx.scene.layout.*
-import javafx.scene.paint.Color
-import javafx.scene.text.*
+import javafx.scene.text.Font
 
-class BoardView(private val model: Model): HBox(), IView {
+class BoardView(private val model: Model): VBox(), IView {
 
+    private var createToDoHeader = Label("Create To-Do item:")
     private var createToDoRowView = CreateToDoRowView(model)
+    private var myToDosHeader = Label("My To-Dos:")
 
     override fun updateView(){
         children.clear()
+
+        createToDoHeader.padding = Insets(0.0, 0.0, 10.0, 0.0)
+        createToDoHeader.font = Font("Regular", 16.0)
+        children.add(createToDoHeader)
+
         children.add(createToDoRowView)
+
+        myToDosHeader.padding = Insets(20.0, 0.0, 10.0, 0.0)
+        myToDosHeader.font = Font("Regular", 16.0)
+        children.add(myToDosHeader)
+        for (item in model.getItems()) {
+            children.add(ToDoRowView(item))
+        }
     }
 
     init {
---
+++ frontend/src/main/kotlin/views/PriorityTagView.kt
@@ -0,0 +1,32 @@
+package views
+import javafx.geometry.Insets
+import javafx.geometry.Pos
+import javafx.scene.control.*
+import javafx.scene.layout.*
+import javafx.scene.paint.Color
+
+class PriorityTagView(priorityLevel: Int): HBox() {
+
+    init {
+        val label = Label()
+        label.text = when (priorityLevel) {
+            0 -> "Low"
+            1 -> "Medium"
+            2 -> "High"
+            else -> ""
+        }
+
+        label.prefWidth = 50.0
+        label.alignment = Pos.CENTER
+
+        val backgroundColor = when (priorityLevel) {
+            0 -> Color.web("#00FF00")
+            1 -> Color.web("#FFA500")
+            2 -> Color.web("#FF0000")
+            else -> Color.web("#FFFFFF")
+        }
+
+        label.background = Background(BackgroundFill(backgroundColor, CornerRadii(5.0), Insets(0.0)))
+        children.add(label)
+    }
+}
